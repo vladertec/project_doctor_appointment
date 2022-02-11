142b8b70d075c043741a049a6a06ac845cb15f32
Alice Moskalenko <alisesavitskaya@gmail.com>
2022-02-11 16:04:36 +0200

    modal-window-add

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,7 +3,7 @@ import getUserToken from "./api/getUserToken.js";
 import getAllUserCards from "./api/getAllUserCards.js";
 import renderInformation from "./codeActions/renderInformation.js";
 import filterFormInputsValues from "./codeActions/filterFormInputsValues.js";
-/*import modalWindowDisplay from "./functions/modalWindowDisplay.js";*/
+import modalWindowDisplay from "./functions/modalWindowDisplay.js";
 import getAuthorizationData from "./codeActions/getAuthorizationData.js";
 import getUserDataToSend from "./codeActions/getUserDataToSend.js";
 
@@ -13,10 +13,9 @@ document.querySelector('.section-menu__btn').addEventListener('click', showModal
 document.querySelector('.form').addEventListener('submit', getAuthorizationData);
 
 document.querySelector('.section-menu__btn-create-visit').addEventListener('click', () => {
-    document.querySelector('.input-form').classList.toggle('show');
-    document.querySelector('.input-form').addEventListener('submit', getUserDataToSend);
+    // document.querySelector('.input-form').classList.toggle('show');
+    // document.querySelector('.input-form').addEventListener('submit', getUserDataToSend);
+    return
 })
-
-
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,19 @@ import getUserToken from "./api/getUserToken.js";
 import getAllUserCards from "./api/getAllUserCards.js";
 import renderInformation from "./codeActions/renderInformation.js";
 import filterFormInputsValues from "./codeActions/filterFormInputsValues.js";
-import modalWindowDisplay from "./functions/modalWindowDisplay.js";
 import getAuthorizationData from "./codeActions/getAuthorizationData.js";
 import getUserDataToSend from "./codeActions/getUserDataToSend.js";
-
+import modalWindowDisplay from "./codeActions/modalWindowDisplay.js"
 
 document.querySelector('.section-menu__btn').addEventListener('click', showModal);
 
 document.querySelector('.form').addEventListener('submit', getAuthorizationData);
 
 document.querySelector('.section-menu__btn-create-visit').addEventListener('click', () => {
-    document.querySelector('.input-form').classList.toggle('show');
-    document.querySelector('.input-form').addEventListener('submit', getUserDataToSend);
+    // document.querySelector('.input-form').classList.toggle('show');
+    // document.querySelector('.input-form').addEventListener('submit', getUserDataToSend);
+    return
 })
 
-modalWindowDisplay()
-
+modalWindowDisplay() 
 
